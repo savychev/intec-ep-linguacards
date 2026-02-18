cdc5a360bdcb02a0aae65892e89fc3270c051f32
savychev <werk3093059@gmail.com>
2026-02-18 15:24:15 +0100

    chore: bootstrap project with MySQL connection

---
+++ src/main/java/be/intecbrussel/linguacards/LinguacardsApplication.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.linguacards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguacardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinguacardsApplication.class, args);
+    }
+
+}
