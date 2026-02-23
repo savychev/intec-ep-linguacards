9427529657839141143066640c7cc668a8c04203
savychev <werk3093059@gmail.com>
2026-02-23 10:54:55 +0100

    init: monorepo skeleton (backend/frontend/docs)

---
+++ backend/linguacards/src/main/java/be/intecbrussel/linguacards/LinguaCardsV2Application.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.linguacards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguaCardsV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinguaCardsV2Application.class, args);
+    }
+
+}
